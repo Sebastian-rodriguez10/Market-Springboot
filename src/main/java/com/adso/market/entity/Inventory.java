9420561f5e723d6b6f98215118bd28f00266eb5d
sebastian rodriguez <juanserodriguez711@gmail.com>
2026-05-27 17:04:33 -0500

    feat: new entity Inventory

---
+++ src/main/java/com/adso/market/entity/Inventory.java
@@ -0,0 +1,20 @@
+package com.adso.market.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private int idProducto;
+}
