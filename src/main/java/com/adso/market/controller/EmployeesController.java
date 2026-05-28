7effb5ac98213a731669c2059a536ccfa366776c
Dana <arrubladana@gmail.com>
2026-05-28 08:01:56 -0500

    feat: create crud the employees

--- src/main/java/com/adso/market/controller/EmployeesController.java
+++ src/main/java/com/adso/market/controller/EmployeesController.java
@@ -1,6 +1,13 @@
 package com.adso.market.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.adso.market.dto.HttpGlobalResponse;
 import com.adso.market.dto.categoryDTO.CategoryNameDTO;
 import com.adso.market.dto.employesDTO.RegisterEmpleoyesDTO;
+import com.adso.market.dto.employesDTO.ResponseEmployeesDTO;
 import com.adso.market.service.EmpleoyeesService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,7 +29,28 @@ public class EmployeesController {
     private final EmpleoyeesService empleoyeesService;
     
     @PostMapping("/post-employes")
-    public HttpGlobalResponse<RegisterEmpleoyesDTO> postEmployes(@RequestBody RegisterEmpleoyesDTO registerEmpleoyesDTO) {
+    public HttpGlobalResponse<RegisterEmpleoyesDTO> postEmployes(@Valid @RequestBody RegisterEmpleoyesDTO registerEmpleoyesDTO) {
         return empleoyeesService.postEmployes(registerEmpleoyesDTO);
     }
+
+    @GetMapping("/get-all")
+    public List<RegisterEmpleoyesDTO> getAllEmployees() {
+        return empleoyeesService.getAllEmployees();
+    }
+
+    @GetMapping("/search/{id}")
+    public HttpGlobalResponse<RegisterEmpleoyesDTO> searchEmployee(@PathVariable Long id) {
+        return empleoyeesService.searchEmployee(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public HttpGlobalResponse<String> deleteEmployee(@PathVariable Long id) {
+    return empleoyeesService.deleteEmployee(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public HttpGlobalResponse<ResponseEmployeesDTO> putEmployee(@PathVariable Long id, @Valid @RequestBody RegisterEmpleoyesDTO updateDTO) {
+    return empleoyeesService.putEmployee(id, updateDTO);
+    }
+
 }
---
+++ src/main/java/com/adso/market/dto/employesDTO/ResponseEmployeesDTO.java
@@ -0,0 +1,18 @@
+package com.adso.market.dto.employesDTO;
+
+import java.time.LocalDate;
+import lombok.Data;
+
+@Data
+public class ResponseEmployeesDTO {
+    
+    private Long id; // Aquí incluimos el ID que no tiene tu otro DTO
+
+    private String identification;
+
+    private String name;
+
+    private String role;
+
+    private LocalDate starDate;
+}
