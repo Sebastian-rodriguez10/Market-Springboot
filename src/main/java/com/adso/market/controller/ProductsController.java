75e7b5a47013b99e18e2497a9387da0438a26161
sebastian rodriguez <juanserodriguez711@gmail.com>
2026-06-01 23:20:16 -0500

    feat: validations controllers

--- src/main/java/com/adso/market/controller/ProductsController.java
+++ src/main/java/com/adso/market/controller/ProductsController.java
@@ -2,6 +2,8 @@ package com.adso.market.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.adso.market.dto.HttpGlobalResponse;
@@ -20,22 +22,42 @@ public class ProductsController {
     private final ProductService productService;
 
     @GetMapping("/get-products")
-    public List<ProductDTO> getProducts(){
-        return productService.getProducts();
+    public ResponseEntity<List<ProductDTO>> getProducts(){
+        List<ProductDTO> response = productService.getProducts();
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
     }
 
     @PostMapping("/post-product")
-    public HttpGlobalResponse<ProductDTO> postProduct(@Valid @RequestBody ProductDTO data){
-        return productService.postProduct(data);
+    public ResponseEntity<HttpGlobalResponse<ProductDTO>> postProduct(@Valid @RequestBody ProductDTO data){
+        HttpGlobalResponse<ProductDTO> response = productService.postProduct(data);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
     }
 
     @PutMapping("/put-product/{id}")
-    public HttpGlobalResponse<ProductDTO> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductDTO data){
-        return productService.updateProduct(id, data);
+    public ResponseEntity<HttpGlobalResponse<ProductDTO>> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductDTO data){
+        HttpGlobalResponse<ProductDTO> response = productService.updateProduct(id, data);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
     }
 
     @DeleteMapping("/delete-product/{id}")
-    public MessageResponseDTO deleteProduct(@PathVariable Long id){
-        return productService.deleteProduct(id);
+    public ResponseEntity<MessageResponseDTO> deleteProduct(@PathVariable Long id){
+        MessageResponseDTO response = productService.deleteProduct(id);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
     }
 }
